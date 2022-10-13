e83169e84b0a3b8bf4f1b69fd4992df165c1a8c1
chiggshiggs <96922117+chiggshiggs@users.noreply.github.com>
2022-10-13 14:09:24 +0530

    Add files via upload

--- components/cardcomponent/Header.js
+++ components/cardcomponent/Header.js
@@ -1,1 +1,16 @@
+import React from 'react'
+import SliderOfCards from "./SliderOfCards"
+import HeaderSt from "./HeaderStyles.module.css"
 
+function Header(){
+    return(
+        <div className={HeaderSt.HeaderContainer}>
+            <div className={HeaderSt.HeaderTitle}>
+                DISCOVER
+            </div>
+            <SliderOfCards />
+        </div>
+    )
+}
+
+export default Header
