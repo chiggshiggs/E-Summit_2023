4a54a53002919f6db17b1c5b8be73ce4210ff77d
chiggshiggs <96922117+chiggshiggs@users.noreply.github.com>
2022-10-14 15:49:30 +0530

    Add files via upload

--- components/cardcomponent/HeaderCard.js
+++ components/cardcomponent/HeaderCard.js
@@ -1,15 +1,59 @@
-import React from "react";
-import HdSt from "./HeaderStyles.module.css";
+import React from 'react'
+import HdSt from './HeaderStyles.module.css'
 
-function HeaderCard({ cardImage, cardTitle }) {
-  return (
-    <div className={HdSt.CardContainer}>
-      <div className={HdSt.CardImage}>
-        {/* <Image src={cardImage} width={640} height={440} /> */}
-      </div>
-      <div className="HdSt.CardTitle">{cardTitle}</div>
-    </div>
-  );
+function HeaderCard(){
+    return(
+        <><><><div className={HdSt.CardContainer}>
+            <div className={HdSt.CardImage}>
+                <Image
+                    src={'./ecell1.png'}
+                    width={640}
+                    height={440} />
+            </div>
+            <div className='HdSt.CardTitle'>
+                Type1
+
+            </div>
+
+        </div><div className={HdSt.CardContainer}>
+                <div className={HdSt.CardImage}>
+                    <Image
+                        src={'./ecell2.png'}
+                        width={640}
+                        height={440} />
+                </div>
+                <div className='HdSt.CardTitle'>
+                    Type2
+
+                </div>
+
+            </div></><div className={HdSt.CardContainer}>
+                <div className={HdSt.CardImage}>
+                    <Image
+                        src={'./ecell3.png'}
+                        width={640}
+                        height={440} />
+                </div>
+                <div className='HdSt.CardTitle'>
+                    Type3
+
+                </div>
+
+            </div></><div className={HdSt.CardContainer}>
+                <div className={HdSt.CardImage}>
+                    <Image
+                        src={'./ecell4.png'}
+                        width={640}
+                        height={440} />
+                </div>
+                <div className='HdSt.CardTitle'>
+                    Type4
+
+                </div>
+
+            </div></>
+    )
+
 }
 
-export default HeaderCard;
+export default HeaderCard
